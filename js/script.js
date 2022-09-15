b62d04d7c3b035cc6636db6c39aa08f1c7b6970c
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-09-15 14:26:21 +0200

    also like section shop detail Pages

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ $(".owl-carousel").owlCarousel({
     autoplayHoverPause:true,
     smartSpeed: 1500,
     loop: true,
-    // center: true,
     responsive: {
         0:{
             items:2
@@ -19,6 +18,30 @@ $(".owl-carousel").owlCarousel({
         },
         1200:{
             items:6
+        }
+    }
+});
+
+
+
+
+$(".product-carousel").owlCarousel({
+    margin:20,
+    autoplay:true,
+    autoplayHoverPause:true,
+    smartSpeed: 1500,
+    loop: true,
+    responsive: {
+        0:{
+            items:1
+        },
+        768:{
+            items:2    
+
+        },
+        992:{
+            items:4          
+
         }
     }
 });
